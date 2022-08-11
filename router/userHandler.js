8a043f67540ae290d3c07a1bc8d228dd998289af
Aniksaha-ui <sahaanik1045@gmail.com>
2022-08-11 11:36:27 +0600

    try to hosting

--- router/userHandler.js
+++ router/userHandler.js
@@ -31,7 +31,7 @@ router.post("/register", async (req, res) => {
   const { email } = req.body;
   const tempPassword = Math.floor(1000 + Math.random() * 9000);
   user.password = tempPassword;
-  const {password} = user;
+  const { password } = user;
   try {
     await user.save();
     const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
@@ -49,57 +49,44 @@ router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const userFound = await User.find({email:email,password:password})
-  if(userFound.length>0){
+  const userFound = await User.find({ email: email, password: password });
+  if (userFound.length > 0) {
     // console.log("found");
-    const token = jwt.sign(
-    { email: email },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "1h" }
-  );
+    const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
 
-  if(token){
-    res.send({status:200, token });
-  }
-   
-
-  }else{
-    res.send({status:404,massage:"data not found"})
+    if (token) {
+      res.send({ status: 200, token });
+    }
+  } else {
+    res.send({ status: 404, massage: "data not found" });
     // console.log("Not found");
   }
-
-  
 });
 
 //get my contact
-router.get("/mycontact" ,async(req,res)=>{
-  
+router.get("/mycontact", async (req, res) => {
   const useremail = req.query.useremail;
   // console.log(useremail);
-  const contact = await Contact.find({useremail:useremail});
+  const contact = await Contact.find({ useremail: useremail });
   // console.log(contact)
- res.send(contact)
-   
-})
+  res.send(contact);
+});
 
 //post a new contact
 
-router.post("/mycontact", async(req,res)=>{
-    // console.log(req.body)
-    const contact = new Contact(req.body);
-    try{
-     const contactInfo = await contact.save();
-     res.send({contact,"message":"data inserted"});
-    } catch{
-
-    }
+router.post("/mycontact", async (req, res) => {
+  // console.log(req.body)
+  const contact = new Contact(req.body);
+  try {
+    const contactInfo = await contact.save();
+    res.send({ contact, message: "data inserted" });
+  } catch {}
 });
 
-
-
 //get all users
-router.get("/",  async (req, res) => {
-  
+router.get("/", async (req, res) => {
   const user = await User.find({});
   if (!user) {
     res.status(400).send({ message: "data not found" });
@@ -108,53 +95,49 @@ router.get("/",  async (req, res) => {
   res.send(user);
 });
 
-async function main(email,password) {
-  
+async function main(email, password) {
   // console.log("function called",email)
- 
+
   var transport = nodemailer.createTransport({
-    host:"smtp.gmail.com",
-    auth:{
-      user:"2018-1-60-181@std.ewubd.edu",
-      pass:"vwgtsqlinehvzaml"
-    }
+    host: "smtp.gmail.com",
+    auth: {
+      user: "2018-1-60-181@std.ewubd.edu",
+      pass: "vwgtsqlinehvzaml",
+    },
   });
 
   var mailOptions = {
     from: "2018-1-60-181@std.ewubd.edu",
-    to:`${email}`,
-    subject:"authentication",
-    text:`Your OTP is ${password}.You can change you password in the following link : http://localhost:4200/register`
-  }
+    to: `${email}`,
+    subject: "authentication",
+    text: `Your OTP is ${password}.You can change you password in the following link : http://localhost:4200/register`,
+  };
 
-  transport.sendMail(mailOptions,function(err,info){
-    if(err){
-      console.log(err)
-    }else{
-      console.log("Email send")
+  transport.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email send");
     }
   });
- 
 }
 
-
 //authorization
-router.get("/authentication/:id",  async (req, res) => {
+router.get("/authentication/:id", async (req, res) => {
   const _id = req.params.id;
-  console.log(_id)
-
-
-  const userUpdateAuth = await User.updateOne({_id
-    : _id}, { authentication: 1 })
-    const user = await User.find({_id:_id})
-    // const {email,password} = user[0];
-    // console.log(email,password);
-    await main(email,password).catch(console.error);
-    if (!userUpdateAuth) {
+  console.log(_id);
+  const userUpdateAuth = await User.updateOne(
+    { _id: _id },
+    { authentication: 1 }
+  );
+  const user = await User.find({ _id: _id });
+  const { email, password } = user[0];
+  // console.log(email,password);
+  await main(email, password).catch(console.error);
+  if (!userUpdateAuth) {
     res.status(400).send({ message: "Authentication is not done" });
   }
   res.send(userUpdateAuth);
 });
-
 
 module.exports = router;
